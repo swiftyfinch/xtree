0252314df768f0ce2e75f343955c12ce6a7b719e
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-04-08 23:22:56 +0500

    Move tree options to option group

--- Sources/XTree/CLI/Commands/Print.swift
+++ Sources/XTree/CLI/Commands/Print.swift
@@ -15,44 +15,8 @@ extension XTree {
         @OptionGroup
         var inputOptions: InputOptions
 
-        @Option(
-            name: .shortAndLong,
-            parsing: .upToNextOption,
-            help: """
-            Keep only subtrees where the root node contains the passed string with wildcards \("(*, ?)".secondary).
-            """
-        )
-        var roots: [String] = []
-
-        @Option(
-            name: .shortAndLong,
-            parsing: .upToNextOption,
-            help: """
-            Keep only the nodes that contain the passed strings with wildcards \("(*, ?)".secondary).
-            """
-        )
-        var contains: [String] = []
-
-        @Option(
-            name: .shortAndLong,
-            parsing: .upToNextOption,
-            help: """
-            Exclude nodes that contain any of the passed strings with wildcards \("(*, ?)".secondary).
-            """
-        )
-        var except: [String] = []
-
-        @Option(name: [.customShort("d", allowingJoined: true), .customLong("depth")],
-                help: "Limit the maximum depth of the tree.")
-        var maxHeight: Int?
-
-        @Option(
-            name: .shortAndLong,
-            help: """
-            Select the sorting method: by name, by number of \("children".secondary) or by \("height".secondary).
-            """
-        )
-        var sort: Sort = .name
+        @OptionGroup
+        var treeOptions: TreeOptions
     }
 }
 
@@ -66,12 +30,12 @@ extension XTree.Print {
         try await treeManager.print(
             inputPath: inputPath,
             filter: .init(
-                roots: roots,
-                contains: contains,
-                except: except,
-                maxHeight: maxHeight
+                roots: treeOptions.roots,
+                contains: treeOptions.contains,
+                except: treeOptions.except,
+                maxHeight: treeOptions.maxHeight
             ),
-            sort: sort
+            sort: treeOptions.sort
         )
     }
 }
