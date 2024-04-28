529724d5243418308df9ac67428a3c08cd5f99bd
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-04-28 22:09:53 +0500

    Escape regex patterns

--- Sources/XTreeKit/Common/RegexBuilder.swift
+++ Sources/XTreeKit/Common/RegexBuilder.swift
@@ -1,8 +1,10 @@
+import Foundation
+
 final class RegexBuilder {
     func build(wildcardsPattern: String) throws -> Regex<Substring> {
-        var pattern = wildcardsPattern
-        pattern = pattern.replacingOccurrences(of: "*", with: ".*")
-        pattern = pattern.replacingOccurrences(of: "?", with: ".")
+        var pattern = NSRegularExpression.escapedPattern(for: wildcardsPattern)
+        pattern = pattern.replacingOccurrences(of: "\\*", with: ".*")
+        pattern = pattern.replacingOccurrences(of: "\\?", with: ".")
         return try Regex("^\(pattern)$")
     }
 }
